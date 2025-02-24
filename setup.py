c87530913c3780cc441e5dd04ad9c19869f85902
Meraj Sarab <merajpython@gmail.com>
2025-02-24 20:56:06 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "zarinpal-payment",
-    version = "0.0.5",
+    version = "0.0.6",
     author = "Meraj Sarab",
     author_email = "srb.meraj@gmail.com",
     description = "a Python package for integrating the ZarinPal payment gateway.",
